8c0b041d654973e51b9a7d768131c4a094f4bacf
Hnrysh <123278267+Hnrysh@users.noreply.github.com>
2025-01-27 21:16:36 -0600

    Trabajo en conexion con SQL

---
+++ Models/ContactMessages.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFormContact.Models
+{
+    public class ContactMessages
+    {
+        public int IdContactMessages { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo electronico es obligatorio")]
+        [EmailAddress(ErrorMessage = "Ingrese Correo Valido!")]
+        [StringLength(75, ErrorMessage = "El correo no puede exceder los 75 caracteres")]
+
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es obligatorio")]
+        [StringLength(150, ErrorMessage = "El mensaje no puede exceder los 150 caracteres")]
+        public string Mensajes { get; set; }
+    }
+}
